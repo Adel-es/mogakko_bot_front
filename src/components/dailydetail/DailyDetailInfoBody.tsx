981386739716f5607d54da1ac0604d176820306d
noonmap <codnjs8221@naver.com>
2022-05-20 18:33:19 +0900

    fix : monthly calendar에서 date를 클릭했을 때 일정 추가 상자를 팝업
    - 노트북과 데탑의 prettier 설정에 어딘가 차이가 있었는지 tab 사이즈가 늘어남

--- src/components/dailydetail/DailyDetailInfoBody.tsx
+++ src/components/dailydetail/DailyDetailInfoBody.tsx
@@ -2,45 +2,43 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import { format } from "date-fns";
 import { useState, useEffect } from "react";
-import CreateScheduleButton from "../schedule/CreateScheduleButton";
-import CreateScheduleBox from "../schedule/CreateScheduleBox";
 import DetailPersonTag from "../persontag/DetailPersonTag";
 import { Schedule } from "../../utils/schedule/ScheduleInfoStruct";
 
 function getTimeString(date: Date): string {
-  const timeString = format(date, "HH") + ":" + format(date, "mm");
-  return timeString;
+	const timeString = format(date, "HH") + ":" + format(date, "mm");
+	return timeString;
 }
 
 interface DailyDetailInfoBodyProp {
-  selectedDay: Date;
-  schedulesOfSelectedDay: Array<Schedule>;
-  onCreateSchedule: any;
+	selectedDay: Date;
+	schedulesOfSelectedDay: Array<Schedule>;
+	onCreateSchedule: any;
 }
 function DailyDetailInfoBody({
-  selectedDay,
-  schedulesOfSelectedDay,
-  onCreateSchedule,
+	selectedDay,
+	schedulesOfSelectedDay,
+	onCreateSchedule,
 }: DailyDetailInfoBodyProp) {
-  const [clickedCreateButton, setClickedCreateButton] = useState(false);
-  const handleClickCreateScheduleButton = () => {
-    setClickedCreateButton((prev) => !prev);
-  };
-  useEffect(() => {
-    setClickedCreateButton(false);
-  }, [selectedDay]);
+	const [clickedCreateButton, setClickedCreateButton] = useState(false);
+	const handleClickCreateScheduleButton = () => {
+		setClickedCreateButton((prev) => !prev);
+	};
+	useEffect(() => {
+		setClickedCreateButton(false);
+	}, [selectedDay]);
 
-  return (
-    <PersonTagAlignCenter>
-      {schedulesOfSelectedDay.map((personInfo: Schedule, index: number) => (
-        <DetailPersonTag
-          key={index} // TODO: index를 나중에 DB에서 id를 받아와서 바꾸기
-          name={personInfo.name}
-          startTime={getTimeString(personInfo.startTime)}
-          endTime={getTimeString(personInfo.endTime)}
-        ></DetailPersonTag>
-      ))}
-      {clickedCreateButton ? (
+	return (
+		<PersonTagAlignCenter>
+			{schedulesOfSelectedDay.map((personInfo: Schedule, index: number) => (
+				<DetailPersonTag
+					key={index} // TODO: index를 나중에 DB에서 id를 받아와서 바꾸기
+					name={personInfo.name}
+					startTime={getTimeString(personInfo.startTime)}
+					endTime={getTimeString(personInfo.endTime)}
+				></DetailPersonTag>
+			))}
+			{/* {clickedCreateButton ? (
         <CreateScheduleBox
           selectedDay={selectedDay}
           onCreateSchedule={onCreateSchedule}
@@ -50,19 +48,19 @@ function DailyDetailInfoBody({
       )}
       <CreateScheduleButton
         onClickCreateScheduleButton={handleClickCreateScheduleButton}
-      ></CreateScheduleButton>
-    </PersonTagAlignCenter>
-  );
+      ></CreateScheduleButton> */}
+		</PersonTagAlignCenter>
+	);
 }
 DailyDetailInfoBody.propTypes = {
-  selectedDay: PropTypes.instanceOf(Date).isRequired,
-  schedulesOfSelectedDay: PropTypes.array,
-  onCreateSchedule: PropTypes.func.isRequired,
+	selectedDay: PropTypes.instanceOf(Date).isRequired,
+	schedulesOfSelectedDay: PropTypes.array,
+	onCreateSchedule: PropTypes.func.isRequired,
 };
 const PersonTagAlignCenter = styled.div`
-  width: 100%;
-  // text-align: center;
-  // display: flex;
-  // align-items: middle;
+	width: 100%;
+	// text-align: center;
+	// display: flex;
+	// align-items: middle;
 `;
 export default DailyDetailInfoBody;
