eb9ac05de0bde97665d4ba004cd109234fa8a8e1
noonmap <codnjs8221@naver.com>
2022-05-26 17:16:24 +0900

    feat : monthly view에서 날짜 slot 클릭 시 일정 생성 가능
    issue : #11

--- src/components/calendar/MonthlyCalendar.tsx
+++ src/components/calendar/MonthlyCalendar.tsx
@@ -4,7 +4,8 @@ import enUS from "date-fns/locale/en-US";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Schedule } from "../../utils/schedule/ScheduleInfoStruct";
 import { EachDateType } from "../../utils/calendar/MonthListGenerator";
-import { useCallback } from "react";
+import { useState, useCallback } from "react";
+import ScheduleInputPopUpBox from "../schedule/ScheduleInputPopUpBox";
 
 const locales = {
 	"en-US": enUS,
@@ -21,36 +22,49 @@ const localizer = dateFnsLocalizer({
 interface MonthlyCalendarProp {
 	calendarOfCurrentMonth: Map<string, EachDateType>;
 	schedulesOfCurrentMonth: Map<number, Schedule>;
-	onClickDayOnCalendar: any;
-	onActiveStartDateChangeOnCalendar: any;
+	onCreateSchedule: any;
 }
 function MonthlyCalendar({
 	calendarOfCurrentMonth,
 	schedulesOfCurrentMonth,
-	onClickDayOnCalendar,
-	onActiveStartDateChangeOnCalendar,
+	onCreateSchedule,
 }: MonthlyCalendarProp) {
+	const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+	const [selected, setSelected] = useState<boolean>(false);
 	const onSelectSlot = useCallback((slotInfo: any) => {
-		/**
-		 * Here we are waiting 250 milliseconds (use what you want) prior to firing
-		 * our method. Why? Because both 'click' and 'doubleClick'
-		 * would fire, in the event of a 'doubleClick'. By doing
-		 * this, the 'click' handler is overridden by the 'doubleClick'
-		 * action.
-		 */
-		onClickDayOnCalendar();
+		setSelectedDate(slotInfo.slots[0]);
+		setSelected(true);
+
+		console.log(selectedDate);
 	}, []);
+
+	const handleCloseSchedulePopupBox = () => {
+		setSelected(false);
+	};
+
 	return (
-		<Calendar
-			localizer={localizer}
-			startAccessor="start"
-			endAccessor="end"
-			style={{ minWidth: 600, height: 800 }}
-			events={Array.from(schedulesOfCurrentMonth.values())}
-			titleAccessor={"name"}
-			onSelectSlot={onSelectSlot}
-			views={["month", "day", "agenda"]}
-		></Calendar>
+		<>
+			<Calendar
+				localizer={localizer}
+				startAccessor="start"
+				endAccessor="end"
+				style={{ minWidth: 600, height: 800 }}
+				events={Array.from(schedulesOfCurrentMonth.values())}
+				titleAccessor={"name"}
+				onSelectSlot={onSelectSlot}
+				selectable
+				views={["month", "day", "agenda"]}
+				popup
+			></Calendar>
+			{selected && (
+				<ScheduleInputPopUpBox
+					defaultContent={{ startDate: selectedDate, endDate: selectedDate }}
+					open={selected}
+					onClose={handleCloseSchedulePopupBox}
+					onSave={onCreateSchedule}
+				></ScheduleInputPopUpBox>
+			)}
+		</>
 	);
 }
 export default MonthlyCalendar;
--- src/pages/ScheduleManager.tsx
+++ src/pages/ScheduleManager.tsx
@@ -1,16 +1,34 @@
 import {
 	Schedule,
-	ScheduleInfoStruct,
 	samplePeopleInfo,
 } from "../utils/schedule/ScheduleInfoStruct";
 import { differenceInCalendarDays, addDays } from "date-fns";
 import { useState } from "react";
 import { GenerateCalendarOfCurrentMonth } from "../utils/calendar/MonthListGenerator";
 import { EachDateType } from "../utils/calendar/MonthListGenerator";
-import ScheduleInputPopUpBox from "../components/schedule/ScheduleInputPopUpBox";
 import MonthlyCalendar from "../components/calendar/MonthlyCalendar";
 
 function mergeCalendarWithSchedule(
+	calendarOfCurrentMonth: Map<string, EachDateType>,
+	schedule: Schedule
+): Map<string, EachDateType> {
+	/**
+	 * @returns : 스케쥴 리스트를 병합한 한 달(선택한 월) 날짜 리스트
+	 */
+	const schedulePeriod = differenceInCalendarDays(schedule.end, schedule.start);
+	const containedDates = [];
+	for (let i = 0; i <= schedulePeriod; i++) {
+		// console.log(" Plus Date: ", addDays(schedule.start, i).toDateString());
+		containedDates.push(addDays(schedule.start, i).toDateString());
+	}
+	for (const date of containedDates) {
+		if (!calendarOfCurrentMonth.has(date)) continue;
+		calendarOfCurrentMonth.get(date)!.schedules.add(schedule.id);
+	}
+	return calendarOfCurrentMonth;
+}
+
+function mergeCalendarWithSchedules(
 	calendarOfCurrentMonth: Map<string, EachDateType>,
 	schedulesOfCurrentMonth: Map<number, Schedule>
 ): Map<string, EachDateType> {
@@ -18,25 +36,17 @@ function mergeCalendarWithSchedule(
 	 * @returns : 스케쥴 리스트를 병합한 한 달(선택한 월) 날짜 리스트
 	 */
 	for (let [key, schedule] of schedulesOfCurrentMonth) {
-		const schedulePeriod = differenceInCalendarDays(
-			schedule.end,
-			schedule.start
+		// TODO: 비효율적인 객체 복사 & 할당을 거치고 있음.
+		calendarOfCurrentMonth = mergeCalendarWithSchedule(
+			calendarOfCurrentMonth,
+			schedule
 		);
-		const containedDates = [];
-		for (let i = 0; i <= schedulePeriod; i++) {
-			// console.log(" Plus Date: ", addDays(schedule.start, i).toDateString());
-			containedDates.push(addDays(schedule.start, i).toDateString());
-		}
-		for (const date of containedDates) {
-			if (!calendarOfCurrentMonth.has(date)) continue;
-			calendarOfCurrentMonth.get(date)!.schedules.add(schedule.id);
-		}
 	}
 	return calendarOfCurrentMonth;
 }
 
 export interface ScheduleEvent {
-	title: string;
+	name: string;
 	start: Date;
 	end: Date;
 	resource: {
@@ -60,7 +70,7 @@ function getSchedulesOnSelectedDay(
 	const schedulesOfSelectedDay = idsOfSelectedDay.map((id) => {
 		const schedule = schedulesOfCurrentMonth.get(id)!;
 		return {
-			title: schedule.name,
+			name: schedule.name,
 			start: schedule.start,
 			end: schedule.end,
 			resource: { title: schedule.title, content: schedule.content },
@@ -72,8 +82,8 @@ function getSchedulesOnSelectedDay(
 
 function StudyManager() {
 	const [startDateOnCalendar, setStartDateOnCalendar] = useState(new Date());
-	const [dayClicked, setDayClicked] = useState(false);
-	const [dayDoubleClicked, setDayDoubleClicked] = useState(false);
+	// const [dayClicked, setDayClicked] = useState(false);
+	// const [dayDoubleClicked, setDayDoubleClicked] = useState(false);
 	const [selectedDay, setSelectedDay] = useState(new Date());
 	const [schedulesOfCurrentMonth, setSchedulesOfCurrentMonth] = useState(
 		new Map(samplePeopleInfo.map((personInfo) => [personInfo.id, personInfo]))
@@ -83,91 +93,52 @@ function StudyManager() {
 	 * calendarOfCurrentMonth 에는 각 day에 schedule의 id를 Set()으로 저장,
 	 * schedulesOfCurrentMonth 에는 schedule을 Map(id, {schedule 정보})에 저장.
 	 */
-	let calendarOfCurrentMonth = mergeCalendarWithSchedule(
+	let calendarOfCurrentMonth = mergeCalendarWithSchedules(
 		GenerateCalendarOfCurrentMonth(startDateOnCalendar),
 		schedulesOfCurrentMonth
 	);
 
-	let schedulesOfSelectedDay = getSchedulesOnSelectedDay(
-		selectedDay,
-		calendarOfCurrentMonth,
-		schedulesOfCurrentMonth
-	);
+	// let schedulesOfSelectedDay = getSchedulesOnSelectedDay(
+	// 	selectedDay,
+	// 	calendarOfCurrentMonth,
+	// 	schedulesOfCurrentMonth
+	// );
 
-	const handleClickDayOnCalendar = ({
-		clickedDay,
-		clickEvent,
-	}: {
-		clickedDay: Date;
-		clickEvent: any;
-	}) => {
-		// setDayClicked(true);
-		switch (clickEvent.detail) {
-			case 1:
-				setDayClicked(true);
-				break;
-			case 2:
-				if (selectedDay.toDateString() === clickedDay.toDateString()) {
-					setDayDoubleClicked((current) => !current);
-				} else {
-					setDayDoubleClicked(true);
-					setSelectedDay(clickedDay);
-					schedulesOfSelectedDay = getSchedulesOnSelectedDay(
-						selectedDay,
-						calendarOfCurrentMonth,
-						schedulesOfCurrentMonth
-					);
-				}
-				break;
-		}
-	};
+	// function addSchedule(schedule: Schedule);
 
-	const handleCloseSchedulePopupBox = () => {
-		setDayClicked(false);
-	};
+	// const handleCloseSchedulePopupBox = () => {
+	// 	setDayClicked(false);
+	// };
 
-	const handleActiveStartDateChangeOnMonthlyCalendar = (prop: {
-		startDate: Date;
-	}) => {
-		setStartDateOnCalendar(prop.startDate);
-	};
-
-	const getSchedulesOnMovedDayOnDailyCalendar = (
-		movedDate: Date
-	): Array<ScheduleEvent> => {
-		return getSchedulesOnSelectedDay(
-			movedDate,
-			calendarOfCurrentMonth,
-			schedulesOfCurrentMonth
-		);
-	};
+	// const handleActiveStartDateChangeOnMonthlyCalendar = (prop: {
+	// 	startDate: Date;
+	// }) => {
+	// 	setStartDateOnCalendar(prop.startDate);
+	// };
 
 	// TODO: create가 안됨.
 	const handleCreateSchedule = (personInfo: Schedule) => {
+		// FIXME: temp code
 		const tempId = schedulesOfCurrentMonth.size + 1;
-		setSchedulesOfCurrentMonth((prev) => new Map(prev).set(tempId, personInfo));
-	};
-	// console.log("Study Manager render");
-	// console.log("schedulesOfCurrentMonth: ", schedulesOfCurrentMonth);
-	// console.log("schedulesOfSelectedDay: ", schedulesOfSelectedDay);
+		personInfo["id"] = tempId;
+		console.log(personInfo);
 
-	// TODO: samplePeopleInfo가 Calendar에 바로 반영이 되지 않고 있음
-	// TODO: 다른 day를 클릭했을 때, create box가 사라지지 않음. 아예 detail view를 없애고 난 후, 다시 열어보면  create box가 사라져있음.
+		calendarOfCurrentMonth = mergeCalendarWithSchedule(
+			calendarOfCurrentMonth,
+			personInfo
+		);
+		setSchedulesOfCurrentMonth((prev) =>
+			new Map(prev).set(personInfo["id"], personInfo)
+		);
+	};
+
 	return (
 		<div>
 			<MonthlyCalendar
 				calendarOfCurrentMonth={calendarOfCurrentMonth}
 				schedulesOfCurrentMonth={schedulesOfCurrentMonth}
-				onClickDayOnCalendar={handleClickDayOnCalendar}
-				onActiveStartDateChangeOnCalendar={
-					handleActiveStartDateChangeOnMonthlyCalendar
-				}
+				onCreateSchedule={handleCreateSchedule}
 			></MonthlyCalendar>
-			{/* <ScheduleInputPopUpBox
-				defaultContent={{ startDate: selectedDay, endDate: selectedDay }}
-				onOpen={dayClicked}
-				onClose={handleCloseSchedulePopupBox}
-			></ScheduleInputPopUpBox> */}
 		</div>
 	);
 }
