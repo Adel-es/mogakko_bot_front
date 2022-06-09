bf32d260b94cd62f5ffc368881506d9a5869abba
noonmap <codnjs8221@naver.com>
2022-06-09 11:21:14 +0900

    test: API test case를 임시로 작성

--- src/components/calendar/ScheduleCalendar.tsx
+++ src/components/calendar/ScheduleCalendar.tsx
@@ -1,5 +1,5 @@
 import { Calendar, dateFnsLocalizer, View } from "react-big-calendar";
-import { format, parse, startOfWeek, getDay } from "date-fns";
+import { format, parse, startOfWeek, getDay, isEqual } from "date-fns";
 import enUS from "date-fns/locale/en-US";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Schedule } from "../../type/CommonInterfaces";
@@ -7,157 +7,204 @@ import { useState, useCallback, useRef, useContext, useEffect } from "react";
 import ScheduleInputPopUpBox from "../schedule/ScheduleInputPopUpBox";
 import { Context, State } from "../../index";
 import {
-  deleteSchedule,
-  getSchedulesByDuration,
-  getSchedulesById,
-  postSchedule,
-  updateSchedule,
-} from "../../utils/api/ScheduleApi";
+	deleteSchedule,
+	getSchedulesByDuration,
+	getSchedulesById,
+	createSchedule,
+	updateSchedule,
+} from "../../utils/api/ScheduleAPI";
 import { getStartAndEndDate } from "../../utils/calendar/CalendarUtils";
+import {
+	createUserProfile,
+	deleteUserProfile,
+	getUserProfile,
+	updateUserProfile,
+} from "../../utils/api/UserAPI";
+import {
+	checkUserIdDuplication,
+	getVerificationCode,
+	submitVerificationCode,
+} from "../../utils/api/SignUpAPI";
+import {
+	destroySession,
+	getSessionCookie,
+	logIn,
+} from "../../utils/api/SignInAPI";
 
 const locales = {
-  "en-US": enUS,
+	"en-US": enUS,
 };
 
 const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
+	format,
+	parse,
+	startOfWeek,
+	getDay,
+	locales,
 });
 
 interface ScheduleCalendarProp {
-  schedulesOfCurrentMonth: Map<number, Schedule>;
-  onCreateSchedule: (schedule: Schedule) => void;
-  onUpdateSchedule: (schedule: Schedule) => void;
-  onDeleteSchedule: (schedule: Schedule) => void;
+	schedulesOfCurrentMonth: Map<number, Schedule>;
+	onCreateSchedule: (schedule: Schedule) => void;
+	onUpdateSchedule: (schedule: Schedule) => void;
+	onDeleteSchedule: (schedule: Schedule) => void;
+	onMoveDate: any;
 }
 function getDefaultSchedule(): Schedule {
-  return {
-    id: 0,
-    title: "",
-    name: "test name", // FIXME: 이름 가져오기
-    start: new Date(),
-    end: new Date(),
-    content: "",
-  };
+	return {
+		id: 0,
+		title: "",
+		name: "test name", // FIXME: 이름 가져오기
+		start: new Date(),
+		end: new Date(),
+		content: "",
+	};
 }
 function ScheduleCalendar({
-  schedulesOfCurrentMonth,
-  onCreateSchedule,
-  onUpdateSchedule,
-  onDeleteSchedule,
+	schedulesOfCurrentMonth,
+	onCreateSchedule,
+	onUpdateSchedule,
+	onDeleteSchedule,
+	onMoveDate,
 }: ScheduleCalendarProp) {
-  const [isSelectSlot, setIsSelectSlot] = useState<boolean>(false);
-  const [isSelectEvent, setIsSelectEvent] = useState<boolean>(false);
-  const [startDay, setStartDay] = useState<Date>(new Date());
-  const [endDay, setEndDay] = useState<Date>(new Date());
-  const defaultPropForScheduleBox = useRef<Schedule>(getDefaultSchedule());
-  const { url }: State = useContext(Context);
-  useEffect(() => {
-    //test
-    const getByDur = getSchedulesByDuration(
-      url,
-      new Date("2022-04-15T09:00:00"),
-      new Date("2022-04-30T09:00:00")
-    );
-    console.log(getByDur);
-    // const postResult = postSchedule(url, {
-    //   start: new Date("2022-04-09T09:00:00"),
-    //   end: new Date("2022-04-09T18:00:00"),
-    //   name: "roonm813",
-    //   title: "this is title",
-    //   content: "this is content",
-    // });
-    // console.log(postResult);
-    // const getById = getSchedulesById(url, 1);
-    // console.log(getById);
-    // const updateResponse = updateSchedule(url, {
-    //   id: 1,
-    //   start: new Date("2022-04-09T09:00:00"),
-    //   end: new Date("2022-04-09T18:00:00"),
-    //   name: "roonm813",
-    //   title: "this is title",
-    //   content: "this is content",
-    // });
-    // console.log(updateResponse);
-    // const deleteResponse = deleteSchedule(url, 1);
-    // console.log(deleteResponse);
-  }, []);
+	const [isSelectSlot, setIsSelectSlot] = useState<boolean>(false);
+	const [isSelectEvent, setIsSelectEvent] = useState<boolean>(false);
+	const [startDay, setStartDay] = useState<Date>(new Date());
+	const [endDay, setEndDay] = useState<Date>(new Date());
+	const defaultPropForScheduleBox = useRef<Schedule>(getDefaultSchedule());
+	const { url }: State = useContext(Context);
+	useEffect(() => {
+		//test
+		// const getByDur = getSchedulesByDuration(
+		// 	url,
+		// 	new Date("2022-04-15T09:00:00"),
+		// 	new Date("2022-04-30T09:00:00")
+		// );
+		// console.log(getByDur);
+		// const postResult = postSchedule(url, {
+		//   start: new Date("2022-04-09T09:00:00"),
+		//   end: new Date("2022-04-09T18:00:00"),
+		//   name: "roonm813",
+		//   title: "this is title",
+		//   content: "this is content",
+		// });
+		// console.log(postResult);
+		// const getById = getSchedulesById(url, 1);
+		// console.log(getById);
+		// const updateResponse = updateSchedule(url, {
+		//   id: 1,
+		//   start: new Date("2022-04-09T09:00:00"),
+		//   end: new Date("2022-04-09T18:00:00"),
+		//   name: "roonm813",
+		//   title: "this is title",
+		//   content: "this is content",
+		// });
+		// console.log(updateResponse);
+		// const deleteResponse = deleteSchedule(url, 1);
+		// console.log(deleteResponse);
+		const userId = "roonm813";
+		const pw = "testpassword";
+		const discordId = "538799426479840000";
+		// USER
+		// console.log(createUserProfile(url, userId, pw, discordId));
+		// console.log(
+		// 	getUserProfile(url, userId).then((response) => response.json())
+		// );
+		// console.log(
+		// 	updateUserProfile(url, userId, pw, "update memo").then((response) =>
+		// 		response.json()
+		// 	)
+		// );
+		// console.log(deleteUserProfile(url, userId));
 
-  useEffect(() => {
-    const response = getSchedulesByDuration(url, startDay, endDay); // userId
-    // defaultPropForScheduleBox.current =
-  }, [startDay, endDay]);
+		// SignUP
+		// console.log(checkUserIdDuplication(url, userId));
+		// console.log(getVerificationCode(url, discordId));
+		// console.log(submitVerificationCode(url, "asdd"));
 
-  const handleSelectSlot = useCallback((slotInfo: any) => {
-    const date = slotInfo.slots[0];
-    defaultPropForScheduleBox.current = getDefaultSchedule();
-    defaultPropForScheduleBox.current.start = date;
-    defaultPropForScheduleBox.current.end = date;
-    setIsSelectSlot(true);
-  }, []);
+		// SignIn
+		// console.log(logIn(url, userId, pw));
+		// console.log(
+		// 	getSessionCookie(url, userId, pw).then((response) =>
+		// 		response.headers.forEach((data) => console.log(data))
+		// 	)
+		// );
+		// console.log(destroySession(url));
+	}, []);
 
-  const handleSelectEvent = useCallback((event: Schedule) => {
-    defaultPropForScheduleBox.current = event;
-    setIsSelectEvent(true);
+	useEffect(() => {
+		onMoveDate(startDay, endDay);
+	}, [startDay, endDay]);
 
-    // console.log(event);
-  }, []);
+	const handleSelectSlot = useCallback((slotInfo: any) => {
+		const date = slotInfo.slots[0];
+		defaultPropForScheduleBox.current = getDefaultSchedule();
+		defaultPropForScheduleBox.current.start = date;
+		defaultPropForScheduleBox.current.end = date;
+		setIsSelectSlot(true);
+	}, []);
 
-  const handleCloseSchedulePopupBox = () => {
-    setIsSelectSlot(false);
-    setIsSelectEvent(false);
-  };
+	const handleSelectEvent = useCallback((event: Schedule) => {
+		defaultPropForScheduleBox.current = event;
+		setIsSelectEvent(true);
 
-  const handleNavigate = (date: Date, view: View) => {
-    // console.log("navigate: ", date.toDateString());
-    // console.log(getStartAndEndDate(date, view));
-    const { start, end } = getStartAndEndDate(date, view);
-    setStartDay(start!);
-    setEndDay(end!);
-  };
+		// console.log(event);
+	}, []);
 
-  return (
-    <>
-      <Calendar
-        localizer={localizer}
-        style={{ minWidth: 600, height: 800 }}
-        events={Array.from(schedulesOfCurrentMonth.values())}
-        startAccessor="start"
-        endAccessor="end"
-        titleAccessor="name"
-        selectable
-        onSelectSlot={handleSelectSlot}
-        onSelectEvent={handleSelectEvent}
-        onNavigate={handleNavigate}
-        views={["month", "day", "agenda"]}
-        popup
-      ></Calendar>
-      {isSelectSlot && (
-        <ScheduleInputPopUpBox
-          defaultSchedule={defaultPropForScheduleBox.current}
-          open={isSelectSlot}
-          readonly={false}
-          onClose={handleCloseSchedulePopupBox}
-          onSave={onCreateSchedule}
-          onUpdate={onUpdateSchedule}
-          onDelete={onDeleteSchedule}
-        ></ScheduleInputPopUpBox>
-      )}
-      {isSelectEvent && (
-        <ScheduleInputPopUpBox
-          defaultSchedule={defaultPropForScheduleBox.current}
-          open={isSelectEvent}
-          readonly={true}
-          onClose={handleCloseSchedulePopupBox}
-          onSave={onCreateSchedule}
-          onUpdate={onUpdateSchedule}
-          onDelete={onDeleteSchedule}
-        ></ScheduleInputPopUpBox>
-      )}
-    </>
-  );
+	const handleCloseSchedulePopupBox = () => {
+		setIsSelectSlot(false);
+		setIsSelectEvent(false);
+	};
+
+	const handleNavigate = (date: Date, view: View) => {
+		// console.log("navigate: ", date.toDateString());
+		// console.log(getStartAndEndDate(date, view));
+		const { start, end } = getStartAndEndDate(date, "month");
+		if (isEqual(startDay, start!) === false) {
+			setStartDay(start!);
+			setEndDay(end!);
+		}
+	};
+
+	return (
+		<>
+			<Calendar
+				localizer={localizer}
+				style={{ minWidth: 600, height: 800 }}
+				events={Array.from(schedulesOfCurrentMonth.values())}
+				startAccessor="start"
+				endAccessor="end"
+				titleAccessor="name"
+				selectable
+				onSelectSlot={handleSelectSlot}
+				onSelectEvent={handleSelectEvent}
+				onNavigate={handleNavigate}
+				views={["month", "day", "agenda"]}
+				popup
+			></Calendar>
+			{isSelectSlot && (
+				<ScheduleInputPopUpBox
+					defaultSchedule={defaultPropForScheduleBox.current}
+					open={isSelectSlot}
+					readonly={false}
+					onClose={handleCloseSchedulePopupBox}
+					onSave={onCreateSchedule}
+					onUpdate={onUpdateSchedule}
+					onDelete={onDeleteSchedule}
+				></ScheduleInputPopUpBox>
+			)}
+			{isSelectEvent && (
+				<ScheduleInputPopUpBox
+					defaultSchedule={defaultPropForScheduleBox.current}
+					open={isSelectEvent}
+					readonly={true}
+					onClose={handleCloseSchedulePopupBox}
+					onSave={onCreateSchedule}
+					onUpdate={onUpdateSchedule}
+					onDelete={onDeleteSchedule}
+				></ScheduleInputPopUpBox>
+			)}
+		</>
+	);
 }
 export default ScheduleCalendar;
--- src/components/schedule/ScheduleInputPopUpBox.tsx
+++ src/components/schedule/ScheduleInputPopUpBox.tsx
@@ -3,122 +3,122 @@ import { Schedule, ScheduleBody } from "../../type/CommonInterfaces";
 import ScheduleInputBox from "./ScheduleInputBox";
 import { useState, useEffect, useContext } from "react";
 import {
-  deleteSchedule,
-  postSchedule,
-  updateSchedule,
-} from "../../utils/api/ScheduleApi";
+	deleteSchedule,
+	createSchedule,
+	updateSchedule,
+} from "../../utils/api/ScheduleAPI";
 import { Context, State } from "../..";
 
 function ScheduleInputPopUpBox({
-  defaultSchedule,
-  open,
-  readonly,
-  onClose,
-  onSave,
-  onUpdate,
-  onDelete,
+	defaultSchedule,
+	open,
+	readonly,
+	onClose,
+	onSave,
+	onUpdate,
+	onDelete,
 }: {
-  defaultSchedule: Schedule;
-  open: boolean;
-  readonly: boolean;
-  onClose: () => void;
-  onSave: (personInfo: Schedule) => void;
-  onUpdate: (personInfo: Schedule) => void;
-  onDelete: (personInfo: Schedule) => void;
+	defaultSchedule: Schedule;
+	open: boolean;
+	readonly: boolean;
+	onClose: () => void;
+	onSave: (personInfo: Schedule) => void;
+	onUpdate: (personInfo: Schedule) => void;
+	onDelete: (personInfo: Schedule) => void;
 }) {
-  const defaultTitle = "";
-  const defaultContent = "";
+	const defaultTitle = "";
+	const defaultContent = "";
 
-  const [title, setTitle] = useState<string>(
-    defaultSchedule.title !== undefined ? defaultSchedule.title : defaultTitle
-  );
-  const [startDate, setStartDate] = useState<Date>(defaultSchedule.start);
-  const [endDate, setEndDate] = useState<Date>(defaultSchedule.end);
-  const [content, setContent] = useState<string>(
-    defaultSchedule.content !== undefined
-      ? defaultSchedule.content
-      : defaultContent
-  );
-  const [isReadOnly, setIsReadOnly] = useState<boolean>(readonly);
-  const { url } = useContext(Context);
+	const [title, setTitle] = useState<string>(
+		defaultSchedule.title !== undefined ? defaultSchedule.title : defaultTitle
+	);
+	const [startDate, setStartDate] = useState<Date>(defaultSchedule.start);
+	const [endDate, setEndDate] = useState<Date>(defaultSchedule.end);
+	const [content, setContent] = useState<string>(
+		defaultSchedule.content !== undefined
+			? defaultSchedule.content
+			: defaultContent
+	);
+	const [isReadOnly, setIsReadOnly] = useState<boolean>(readonly);
+	const { url } = useContext(Context);
 
-  useEffect(() => {
-    setIsReadOnly(readonly);
-  }, [readonly]);
+	useEffect(() => {
+		setIsReadOnly(readonly);
+	}, [readonly]);
 
-  const handleClickUpdateButton = () => {
-    setIsReadOnly(false);
-  };
+	const handleClickUpdateButton = () => {
+		setIsReadOnly(false);
+	};
 
-  const handleSave = () => {
-    const newSchedule: ScheduleBody = {
-      name: defaultSchedule.name,
-      start: startDate,
-      end: endDate,
-      title: title === "" ? defaultTitle : title,
-      content: content === "" ? defaultContent : content,
-    };
-    const response = postSchedule(url, newSchedule);
-    // console.log(response);
+	const handleSave = () => {
+		const newSchedule: ScheduleBody = {
+			name: defaultSchedule.name,
+			start: startDate,
+			end: endDate,
+			title: title === "" ? defaultTitle : title,
+			content: content === "" ? defaultContent : content,
+		};
+		const response = createSchedule(url, newSchedule);
+		// console.log(response);
 
-    onSave({ id: defaultSchedule.id, ...newSchedule }); // FIXME: id 받아오기
-    onClose();
-  };
+		onSave({ id: defaultSchedule.id, ...newSchedule }); // FIXME: id 받아오기
+		onClose();
+	};
 
-  const handleUpdate = () => {
-    const newSchedule: Schedule = {
-      id: defaultSchedule.id,
-      name: defaultSchedule.name,
-      start: startDate,
-      end: endDate,
-      title: title === "" ? defaultTitle : title,
-      content: content === "" ? defaultContent : content,
-    };
-    const response = updateSchedule(url, newSchedule);
+	const handleUpdate = () => {
+		const newSchedule: Schedule = {
+			id: defaultSchedule.id,
+			name: defaultSchedule.name,
+			start: startDate,
+			end: endDate,
+			title: title === "" ? defaultTitle : title,
+			content: content === "" ? defaultContent : content,
+		};
+		const response = updateSchedule(url, newSchedule);
 
-    onUpdate(newSchedule);
-    // onClose();
-    setIsReadOnly(true);
-  };
+		onUpdate(newSchedule);
+		// onClose();
+		setIsReadOnly(true);
+	};
 
-  const handleDelete = () => {
-    const response = deleteSchedule(url, defaultSchedule.id);
+	const handleDelete = () => {
+		const response = deleteSchedule(url, defaultSchedule.id);
 
-    onDelete(defaultSchedule);
-    onClose();
-  };
+		onDelete(defaultSchedule);
+		onClose();
+	};
 
-  return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      sx={{
-        width: "100%",
-        minWidth: 800,
-        maxWidth: 1000,
-      }}
-    >
-      <ScheduleInputBox
-        title={{ value: title, setValue: setTitle }}
-        startDate={{ value: startDate, setValue: setStartDate }}
-        endDate={{ value: endDate, setValue: setEndDate }}
-        content={{ value: content, setValue: setContent }}
-        readonly={isReadOnly}
-      ></ScheduleInputBox>
-      <DialogActions>
-        {isReadOnly && <Button onClick={handleClickUpdateButton}>수정</Button>}
-        {isReadOnly && <Button onClick={handleDelete}>삭제</Button>}
-        {!isReadOnly && (
-          <Button
-            onClick={defaultSchedule.id === 0 ? handleSave : handleUpdate}
-          >
-            저장
-          </Button>
-        )}
-        <Button onClick={onClose}>{isReadOnly ? "닫기" : "취소"}</Button>
-      </DialogActions>
-    </Dialog>
-  );
+	return (
+		<Dialog
+			open={open}
+			onClose={onClose}
+			sx={{
+				width: "100%",
+				minWidth: 800,
+				maxWidth: 1000,
+			}}
+		>
+			<ScheduleInputBox
+				title={{ value: title, setValue: setTitle }}
+				startDate={{ value: startDate, setValue: setStartDate }}
+				endDate={{ value: endDate, setValue: setEndDate }}
+				content={{ value: content, setValue: setContent }}
+				readonly={isReadOnly}
+			></ScheduleInputBox>
+			<DialogActions>
+				{isReadOnly && <Button onClick={handleClickUpdateButton}>수정</Button>}
+				{isReadOnly && <Button onClick={handleDelete}>삭제</Button>}
+				{!isReadOnly && (
+					<Button
+						onClick={defaultSchedule.id === 0 ? handleSave : handleUpdate}
+					>
+						저장
+					</Button>
+				)}
+				<Button onClick={onClose}>{isReadOnly ? "닫기" : "취소"}</Button>
+			</DialogActions>
+		</Dialog>
+	);
 }
 
 export default ScheduleInputPopUpBox;
