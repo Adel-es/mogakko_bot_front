3fc800acf681f1bbd30b35c452d2a3dc7603d6cf
noonmap <codnjs8221@naver.com>
2022-06-10 02:21:03 +0900

    feat: 로그인 API 적용
    issue: #16

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,29 +1,38 @@
-import { useState, useEffect } from "react";
 import { Button, Grid, TextField } from "@mui/material";
+import { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Context } from "..";
+import { logIn } from "../utils/api/SignInAPI";
 function SignIn() {
-  const [discordID, setDiscordID] = useState("");
-  const [password, setPassword] = useState("");
-  const [signIn, setSignIn] = useState(false);
-  const navigate = useNavigate();
-  const onChangeID = (event: any) => {
-    console.log("(debugging) : " + event.target.value);
-    setDiscordID(event.target.value);
-  };
-  const onChangePassword = (event: any) => {
-    console.log("(debugging) : " + event.target.value);
-    setPassword(event.target.value);
-  };
-  const onClickSignIn = () => {
-    setSignIn(true);
-    console.log(signIn);
-  };
-  useEffect(() => {
-    if (true === signIn) {
-      navigate("/schedule");
-    }
-  }, [signIn]);
-    return (
+	const [discordID, setDiscordID] = useState("");
+	const [password, setPassword] = useState("");
+	const [signIn, setSignIn] = useState(false);
+	const { url } = useContext(Context);
+	const navigate = useNavigate();
+	const onChangeID = (event: any) => {
+		// console.log("(debugging) : " + event.target.value);
+		setDiscordID(event.target.value);
+	};
+	const onChangePassword = (event: any) => {
+		// console.log("(debugging) : " + event.target.value);
+		setPassword(event.target.value);
+	};
+	const onClickSignIn = () => {
+		const response = logIn(url, discordID, password).then((response) => {
+			if (response.status === 200) setSignIn(true);
+			return response.status;
+		});
+		// console.log(response);
+		// console.log(discordID);
+		// console.log(password);
+	};
+	useEffect(() => {
+		if (true === signIn) {
+			navigate("/schedule");
+			console.log(signIn);
+		}
+	}, [signIn]);
+	return (
 		<Grid
 			container
 			alignItems="center"
@@ -35,8 +44,8 @@ function SignIn() {
 				<TextField
 					label="아이디"
 					variant="outlined"
-            value={discordID}
-            onChange={onChangeID}
+					value={discordID}
+					onChange={onChangeID}
 				></TextField>
 			</Grid>
 			<Grid item>
@@ -44,8 +53,8 @@ function SignIn() {
 					label="비밀번호"
 					variant="outlined"
 					type="password"
-            value={password}
-            onChange={onChangePassword}
+					value={password}
+					onChange={onChangePassword}
 				></TextField>
 			</Grid>
 			<Grid item>
@@ -57,6 +66,6 @@ function SignIn() {
 				<Link to="/signup">처음 방문하셨다면 회원가입을 해주세요</Link>
 			</Grid>
 		</Grid>
-  );
+	);
 }
 export default SignIn;
