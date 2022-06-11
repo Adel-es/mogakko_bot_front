e07131e7452fea90e743714259ae66a6830c7f3c
noonmap <codnjs8221@naver.com>
2022-06-12 01:44:54 +0900

    feat: 회원가입 페이지 mui로 새단장
    - 비밀번호 확인 문구와 비밀번호가 다르면 TextField에 error가 뜸
    
    issue : #18

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,109 +1,188 @@
+import { Button, Grid, TextField, Box, createTheme } from "@mui/material";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-function SignUp() {
-  const [discordID, setDiscordID] = useState("");
-  const [isCorrectID, setIsCorrectID] = useState(false);
-  const [certificationNum, setCertificationNum] = useState("");
-  const [isCorrectCertNum, setIsCorrectCertNum] = useState(false);
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPasswor] = useState("");
-  const [register, setRegister] = useState(false);
 
-  const onChangeID = (event: any) => {
-    console.log("(debugging)" + event.target.value);
-    // setDiscordID(state.value);
-  };
-  const onClickID = () => {
-    setIsCorrectID(true);
-  };
-  const onChangeCertNum = (event: any) => {
-    console.log("(debugging)" + event.target.value);
-  };
-  const onClickCertNum = () => setIsCorrectCertNum(true);
-  const onChangePassword = (event: any) => {
-    console.log("(debugging)" + event.target.value);
-  };
-  const onChangeConfirmPassword = (event: any) => {
-    console.log("(debugging)" + event.target.value);
-  };
-  const onClickRegister = () => setRegister(true);
-  function GetDiscordID() {
-    // function checkCorrectID() {}
-    return (
-      <div>
-        <p>인증번호를 받을 DiscordID를 입력하세요</p>
-        <input
-          value={discordID}
-          placeholder="디스코드 ID"
-          onChange={onChangeID}
-        ></input>
-        <button onClick={onClickID}>다음 단계</button>
-      </div>
-    );
-  }
-  function PutCertificationNum() {
-    // function CheckCertificationNum() {}
-    return (
-      <div>
-        <input
-          value={discordID}
-          placeholder="디스코드 ID"
-          disabled={true}
-        ></input>
-        <input
-          value={certificationNum}
-          placeholder="인증번호"
-          onChange={onChangeCertNum}
-        ></input>
-        <button onClick={onClickCertNum}>인증번호 확인</button>
-      </div>
-    );
-  }
-  function GetNewPassword() {
-    return (
-      <div>
-        <input
-          value={discordID}
-          placeholder="디스코드 ID"
-          disabled={true}
-        ></input>
-        <input
-          value={password}
-          placeholder="비밀번호"
-          onChange={onChangePassword}
-        ></input>
-        <input
-          value={confirmPassword}
-          placeholder="비밀번호 확인"
-          onChange={onChangeConfirmPassword}
-        ></input>
-        <button onClick={onClickRegister}>회원가입</button>
-      </div>
-    );
-  }
-  function ChangeForm() {
-    console.log("isCorrectID: " + isCorrectID);
-    console.log("isCorrectCertNum: " + isCorrectCertNum);
-    if (false === isCorrectID) {
-      return <GetDiscordID></GetDiscordID>;
-    }
-    if (false === isCorrectCertNum) {
-      return <PutCertificationNum></PutCertificationNum>;
-    }
-    if (false === register) {
-      return <GetNewPassword></GetNewPassword>;
-    }
-    return (
-      <div>
-        <p>회원가입을 완료하였습니다.</p>
-        <Link to="/signin">로그인 창으로 이동하기</Link>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <ChangeForm></ChangeForm>
-    </div>
-  );
+const enum CertNumState {
+	NoCheck = 3,
+	WrongNum = 1,
+	CorrectNum = 2,
+}
+const enum ConfirmPasswordState {
+	NoTyping = 0,
+	WrongPassword = 1,
+	CorrectPassword = 2,
+}
+function SignUp() {
+	const [discordID, setDiscordID] = useState("");
+	const [requestCertNum, setRequestCertNum] = useState(false);
+	const [certificationNum, setCertificationNum] = useState("");
+	const [isCorrectCertNum, setIsCorrectCertNum] = useState<CertNumState>(
+		CertNumState.NoCheck
+	);
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [confirmPasswordState, setConfirmPasswordState] =
+		useState<ConfirmPasswordState>(ConfirmPasswordState.NoTyping);
+	const [register, setRegister] = useState(false);
+
+	const handleChangeDiscordID = (event: any) => {
+		setDiscordID(event.target.value);
+	};
+	const handleClickCorrectID = () => {
+		setRequestCertNum(true);
+	};
+	const handleChangeCertNum = (event: any) => {
+		setCertificationNum(event.target.value);
+	};
+	const handleChangePassword = (event: any) => {
+		setPassword(event.target.value);
+		if (confirmPasswordState !== ConfirmPasswordState.NoTyping) {
+			if (event.target.value === confirmPassword) {
+				setConfirmPasswordState(ConfirmPasswordState.CorrectPassword);
+			} else {
+				setConfirmPasswordState(ConfirmPasswordState.WrongPassword);
+			}
+		}
+	};
+	const handleChangeConfirmPassword = (event: any) => {
+		setConfirmPassword(event.target.value);
+		if (event.target.value === password) {
+			setConfirmPasswordState(ConfirmPasswordState.CorrectPassword);
+		} else {
+			setConfirmPasswordState(ConfirmPasswordState.WrongPassword);
+		}
+	};
+	const handlelickRegister = () => {
+		// TODO: 회원 가입 버튼 눌렀을 때 넘어가는 조건 작성
+		// setIsCorrectCertNum(true);
+		if (true) {
+			// api 호출
+			setIsCorrectCertNum(CertNumState.CorrectNum);
+		} else {
+			setIsCorrectCertNum(CertNumState.WrongNum);
+		}
+		if (confirmPasswordState === ConfirmPasswordState.CorrectPassword)
+			setRegister(true);
+	};
+
+	return (
+		<Box
+			display="flex"
+			justifyContent="center" // 가로 정렬
+			minHeight="90vh"
+			maxHeight="100vh"
+		>
+			{!register && (
+				<Grid
+					container
+					justifyContent="center" // 세로 정렬
+					direction="column"
+					spacing={2}
+					maxWidth="400px"
+				>
+					<Grid
+						item
+						container
+						spacing={2}
+						alignItems="stretch" // 하위의 버튼 높이 stretch
+					>
+						<Grid item xs={12} sm={7}>
+							<TextField
+								value={discordID}
+								placeholder={"디스코드 ID"}
+								onChange={handleChangeDiscordID}
+								disabled={requestCertNum}
+								fullWidth
+							/>
+						</Grid>
+						<Grid item container xs={12} sm={5} alignItems="stretch">
+							<Button
+								variant="outlined"
+								onClick={handleClickCorrectID}
+								fullWidth
+							>
+								인증번호 받기
+							</Button>
+						</Grid>
+					</Grid>
+					<Grid item>
+						<TextField
+							value={certificationNum}
+							placeholder="인증번호"
+							onChange={handleChangeCertNum}
+							fullWidth
+						/>
+					</Grid>
+					<Grid item container spacing={2}>
+						<Grid item xs={12}>
+							<TextField
+								value={password}
+								placeholder="비밀번호"
+								onChange={handleChangePassword}
+								type="password"
+								fullWidth
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								value={confirmPassword}
+								placeholder="비밀번호 확인"
+								onChange={handleChangeConfirmPassword}
+								type="password"
+								error={
+									confirmPasswordState === ConfirmPasswordState.WrongPassword
+								}
+								color={
+									confirmPasswordState === ConfirmPasswordState.CorrectPassword
+										? "success"
+										: "primary"
+								}
+								helperText={
+									confirmPasswordState === ConfirmPasswordState.WrongPassword
+										? "비밀번호가 일치하지 않습니다."
+										: ""
+								}
+								fullWidth
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<Button
+								variant="contained"
+								onClick={handlelickRegister}
+								fullWidth
+							>
+								회원 가입
+							</Button>
+						</Grid>
+					</Grid>
+				</Grid>
+			)}
+			{requestCertNum &&
+				isCorrectCertNum &&
+				register && ( // 가입 완료되었으면 나타남
+					<Grid
+						container
+						direction="column"
+						spacing={10}
+						justifyContent="center"
+						alignContent={"center"}
+					>
+						<Grid item>
+							<Box style={{ fontSize: "20px" }}>회원가입을 완료하였습니다.</Box>
+						</Grid>
+						<Grid item>
+							<Button
+								fullWidth
+								variant="outlined"
+								component={Link}
+								to={"/signin"}
+							>
+								로그인 창으로 이동하기
+							</Button>
+						</Grid>
+					</Grid>
+				)}
+		</Box>
+	);
 }
 export default SignUp;
