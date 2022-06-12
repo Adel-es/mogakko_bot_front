d9724bc27340f3d8975054f632776c3add2d54d5
noonmap <codnjs8221@naver.com>
2022-06-12 17:50:07 +0900

    feat: Home을 mui로 새단장
    - discord 아이콘 추가
    - 상단 AppBar 추가
    
    issue: #19

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,45 @@
+import {
+	AppBar,
+	Box,
+	Button,
+	CssBaseline,
+	SvgIcon,
+	Toolbar,
+} from "@mui/material";
+
 import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../static/icons/discord_icon_logo.svg";
 function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/signin">
-        <button>Sign In</button>
-      </Link>
-      <Link to="/signup">
-        <button>Sign Up</button>
-      </Link>
-    </div>
-  );
+	return (
+		<>
+			<CssBaseline />
+			<AppBar position="static" sx={{ flexGrow: 1 }}>
+				<Toolbar>
+					<Box display="flex" alignItems="center" sx={{ flexGrow: 1 }}>
+						<SvgIcon>
+							<Logo />
+						</SvgIcon>
+					</Box>
+					<Button
+						color="inherit"
+						variant="outlined"
+						component={Link}
+						to={"/signup"}
+						sx={{ my: 1, mx: 1.5 }}
+					>
+						회원가입
+					</Button>
+					<Button
+						color="inherit"
+						component={Link}
+						to={"/signin"}
+						sx={{ my: 1, mx: 1.5 }}
+					>
+						로그인
+					</Button>
+				</Toolbar>
+			</AppBar>
+		</>
+	);
 }
 export default Home;
