046acf6fcc56729e07b15da85cd06405c54f9450
noonmap <codnjs8221@naver.com>
2022-06-12 16:07:54 +0900

    fix : Home 폴더 이동
    issue: #19

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "./index";
-import Home from "./router/Home";
+import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ScheduleManager from "./pages/ScheduleManager";
 
 function App() {
-  return (
-    <Provider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signin" element={<SignIn />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/schedule" element={<ScheduleManager />}></Route>
-        </Routes>
-      </Router>
-    </Provider>
-  );
+	return (
+		<Provider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/signin" element={<SignIn />}></Route>
+					<Route path="/signup" element={<SignUp />}></Route>
+					<Route path="/schedule" element={<ScheduleManager />}></Route>
+				</Routes>
+			</Router>
+		</Provider>
+	);
 }
 
 export default App;
